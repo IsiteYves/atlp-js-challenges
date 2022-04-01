ada05139908571ba43c7c0da0f620b7de77e16e9
IsiteYves <yvesisite@gmail.com>
2022-03-31 17:33:30 -0700

    Completed checking  palindromes without using built-in functions

--- basic/palindrome.js
+++ basic/palindrome.js
@@ -1,7 +1,17 @@
+// WITH NO BUILT-IN FUNCTIONS
 function isPalindrome(str) {
-    if(str.split('').reverse().join('')==str)
-        return true;
-    return false;
+    let i, n=str.length;
+    for(i=0;i<n/2;i++)
+        if(str[i] != str[n-1-i])
+            return false;
+    return true;
 }
+
+// USING BUILT-IN FUNCTIONS
+// function isPalindrome(str) {
+//     if(str.split('').reverse().join('')==str)
+//         return true;
+//     return false;
+// }
 console.log(isPalindrome("hello"));
 console.log(isPalindrome("madam"));
